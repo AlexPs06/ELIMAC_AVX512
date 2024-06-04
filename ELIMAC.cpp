d620683462fa0e72f6d3390ee541c76158fb97fb
AlexPs06 <luis_pesar@hotmail.com>
2024-06-04 12:18:13 -0600

    se coprrigieron errores de generacion del tag

--- ELIMAC.cpp
+++ ELIMAC.cpp
@@ -28,174 +28,175 @@ static void AES_cast_128_to_512_key2(__m128i *key,__m512i *key_512);
 static inline void AES_encrypt(__m128i tmp, __m128i *out,__m128i *key, unsigned rounds);
 static void imprimiArreglo(int tam, unsigned char *in );
 void H_128(__m128i * nonce,  __m128i *key, unsigned rounds,unsigned nblks);
-void I_128(__m128i * nonce,  __m128i *key, unsigned rounds,unsigned nblks);
-int main(){
+void I_128(__m128i * nonce,  __m128i  key, unsigned rounds,unsigned nblks);
 
-    ALIGN(64) unsigned char plaintext[128]=  {
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 
+// int main(){
+
+//     ALIGN(64) unsigned char plaintext[128]=  {
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 
                                              
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-                                             0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+//                                              0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 
 
-                                            };
-    ALIGN(16) unsigned char tag[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
-    ALIGN(16) unsigned char K_1[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
-    ALIGN(16) unsigned char K_2[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+//                                             };
+//     ALIGN(16) unsigned char tag[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+//     ALIGN(16) unsigned char K_1[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+//     ALIGN(16) unsigned char K_2[16 ]={ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
 
-    ELIMAC(K_1, K_2, plaintext, 128, tag);
+//     ELIMAC(K_1, K_2, plaintext, 128, tag);
 
-    printf("\n");
-    imprimiArreglo(16, tag);
-    return 0;
-}
-
-//  char infoString[]= "EliMAC AVX512";  /* Each AE implementation must have a global one */
-
-// #ifndef MAX_ITER
-// #define MAX_ITER 16384
-// #endif
-
-// int main(int argc, char **argv)
-// {
-// 	/* Allocate locals */
-// 	ALIGN(64) unsigned char pt[MAX_ITER] = {0};
-// 	ALIGN(16) unsigned char key[16]={ 0x00,0x01,0x02,0x03,
-//                                        0x04,0x05,0x06,0x07,
-//                                        0x08,0x09,0x0a,0x0b,
-//                                        0x0c,0x0d,0x0e,0x0f};
-//     ALIGN(16) unsigned char key_2[16]={ 0x00,0x01,0x02,0x03,
-//                                        0x04,0x05,0x06,0x07,
-//                                        0x08,0x09,0x0a,0x0b,
-//                                        0x0c,0x0d,0x0e,0x0f};
-
-//      ALIGN(16) unsigned char tag[16 ]={ 0x00,0x01,0x02,0x03,
-//                                        0x04,0x05,0x06,0x07,
-//                                        0x08,0x09,0x0a,0x0b,
-//                                        0x0c,0x0d,0x0e,0x0f};
-//     ALIGN(16) unsigned char nonce[16 ]={ 0x00,0x01,0x02,0x03,
-//                                        0x04,0x05,0x06,0x07,
-//                                        0x08,0x09,0x0a,0x0b,
-//                                        0x0c,0x0d,0x0e,0x0f};
-// 	char outbuf[MAX_ITER*15+1024];
-// 	int iter_list[2048]; /* Populate w/ test lengths, -1 terminated */
-// 	char *outp = outbuf;
-// 	int iters, i, j, len;
-// 	double Hz,sec;
-// 	double ipi=0, tmpd;
-// 	clock_t c;
-// 	iter_list[0] = 64;
-// 	iter_list[1] = 128;
-// 	iter_list[2] = 256;
-// 	iter_list[3] = 512;
-// 	iter_list[4] = 1024;
-// 	iter_list[5] = 2048;
-// 	iter_list[6] = 4096;
-// 	iter_list[7] = 8192;
-// 	iter_list[8] = 16384;
-// 	iter_list[9] = -1;
-//     /* Create file for writing data */
-// 	FILE *fp = NULL;
-//     char str_time[25];
-// 	time_t tmp_time = time(NULL);
-// 	struct tm *tp = localtime(&tmp_time);
-// 	strftime(str_time, sizeof(str_time), "%F %R", tp);
-// 	if ((argc < 2) || (argc > 3)) {
-// 		printf("Usage: %s MHz [output_filename]\n", argv[0]);
-// 		return 0;
-// 	} else {
-// 		Hz = 1e6 * strtol(argv[1], (char **)NULL, 10);
-// 		if (argc == 3)
-// 			fp = fopen(argv[2], "w");
-// 	}
-
-	
-//     outp += sprintf(outp, "%s ", infoString);
-
-//     outp += sprintf(outp, "- Run %s\n\n",str_time);
-
-// 	// outp += sprintf(outp, "Context: %d bytes\n");
-    
-// 	printf("Starting run...\n");fflush(stdout);
-
-
-// 	/*
-// 	 * Get time for key setup
-// 	 */
-// 	// iters = (int)(Hz/520);
-// 	// do {
-	
-// 	// 	c = clock();
-// 	// 	for (j = 0; j < iters; j++) {
-
-// 	// 	}
-// 	// 	c = clock() - c;
-// 	// 	sec = c/(double)CLOCKS_PER_SEC;
-// 	// 	tmpd = (sec * Hz) / (iters);
-		
-// 	// 	if ((sec < 1.2)||(sec > 1.3))
-// 	// 		iters = (int)(iters * 5.0/(4.0 * sec));
-// 	// 	printf("%f\n", sec);
-// 	// } while ((sec < 1.2) || (sec > 1.3));
-
-	
-// 	printf("key -- %.2f (%d cycles)\n",sec,(int)tmpd);fflush(stdout);
-// 	outp += sprintf(outp, "Key setup: %d cycles\n\n", (int)tmpd);
-
-// 	/*
-// 	 * Get times over different lengths
-// 	 */
-// 	iters = (int)(Hz/1000);
-// 	i=0;
-// 	len = iter_list[0];
-// 	while (len >= 0) {
-	
-// 		do {
-		
-
-//             ELIMAC(key, key_2, pt,iter_list[i], tag);
-
-// 			c = clock();
-// 			for (j = 0; j < iters; j++) {
-//                 ELIMAC(key, key_2, pt,iter_list[i], tag);
-// 			}
-// 			c = clock() - c;
-// 			sec = c/(double)CLOCKS_PER_SEC;
-// 			tmpd = (sec * Hz) / ((double)len * iters);
-			
-// 			if ((sec < 1.2)||(sec > 1.3))
-// 				iters = (int)(iters * 5.0/(4.0 * sec));
-			
-// 		} while ((sec < 1.2) || (sec > 1.3));
-		
-// 		printf("%d -- %.2f  (%6.2f cpb)\n",len,sec,tmpd);fflush(stdout);
-// 		outp += sprintf(outp, "%5d  %6.2f\n", len, tmpd);
-// 		if (len==44) {
-// 			ipi += 0.05 * tmpd;
-// 		} else if (len==552) {
-// 			ipi += 0.15 * tmpd;
-// 		} else if (len==576) {
-// 			ipi += 0.2 * tmpd;
-// 		} else if (len==1500) {
-// 			ipi += 0.6 * tmpd;
-// 		}
-		
-// 		++i;
-// 		len = iter_list[i];
-// 	}
-// 	outp += sprintf(outp, "ipi %.2f\n", ipi);
-// 	if (fp) {
-//         fprintf(fp, "%s", outbuf);
-//         fclose(fp);
-//     } else
-//         fprintf(stdout, "%s", outbuf);
-
-// 	return ((pt[0]==12) && (pt[10]==34) && (pt[20]==56) && (pt[30]==78));
+//     printf("\n");
+//     imprimiArreglo(16, tag);
+//     return 0;
 // }
+
+ char infoString[]= "EliMAC AVX512";  /* Each AE implementation must have a global one */
+
+#ifndef MAX_ITER
+#define MAX_ITER 16384
+#endif
+
+int main(int argc, char **argv)
+{
+	/* Allocate locals */
+	ALIGN(64) unsigned char pt[MAX_ITER] = {0};
+	ALIGN(16) unsigned char key[16]={ 0x00,0x01,0x02,0x03,
+                                       0x04,0x05,0x06,0x07,
+                                       0x08,0x09,0x0a,0x0b,
+                                       0x0c,0x0d,0x0e,0x0f};
+    ALIGN(16) unsigned char key_2[16]={ 0x00,0x01,0x02,0x03,
+                                       0x04,0x05,0x06,0x07,
+                                       0x08,0x09,0x0a,0x0b,
+                                       0x0c,0x0d,0x0e,0x0f};
+
+     ALIGN(16) unsigned char tag[16 ]={ 0x00,0x01,0x02,0x03,
+                                       0x04,0x05,0x06,0x07,
+                                       0x08,0x09,0x0a,0x0b,
+                                       0x0c,0x0d,0x0e,0x0f};
+    ALIGN(16) unsigned char nonce[16 ]={ 0x00,0x01,0x02,0x03,
+                                       0x04,0x05,0x06,0x07,
+                                       0x08,0x09,0x0a,0x0b,
+                                       0x0c,0x0d,0x0e,0x0f};
+	char outbuf[MAX_ITER*15+1024];
+	int iter_list[2048]; /* Populate w/ test lengths, -1 terminated */
+	char *outp = outbuf;
+	int iters, i, j, len;
+	double Hz,sec;
+	double ipi=0, tmpd;
+	clock_t c;
+	iter_list[0] = 64;
+	iter_list[1] = 128;
+	iter_list[2] = 256;
+	iter_list[3] = 512;
+	iter_list[4] = 1024;
+	iter_list[5] = 2048;
+	iter_list[6] = 4096;
+	iter_list[7] = 8192;
+	iter_list[8] = 16384;
+	iter_list[9] = -1;
+    /* Create file for writing data */
+	FILE *fp = NULL;
+    char str_time[25];
+	time_t tmp_time = time(NULL);
+	struct tm *tp = localtime(&tmp_time);
+	strftime(str_time, sizeof(str_time), "%F %R", tp);
+	if ((argc < 2) || (argc > 3)) {
+		printf("Usage: %s MHz [output_filename]\n", argv[0]);
+		return 0;
+	} else {
+		Hz = 1e6 * strtol(argv[1], (char **)NULL, 10);
+		if (argc == 3)
+			fp = fopen(argv[2], "w");
+	}
+
+	
+    outp += sprintf(outp, "%s ", infoString);
+
+    outp += sprintf(outp, "- Run %s\n\n",str_time);
+
+	// outp += sprintf(outp, "Context: %d bytes\n");
+    
+	printf("Starting run...\n");fflush(stdout);
+
+
+	/*
+	 * Get time for key setup
+	 */
+	// iters = (int)(Hz/520);
+	// do {
+	
+	// 	c = clock();
+	// 	for (j = 0; j < iters; j++) {
+
+	// 	}
+	// 	c = clock() - c;
+	// 	sec = c/(double)CLOCKS_PER_SEC;
+	// 	tmpd = (sec * Hz) / (iters);
+		
+	// 	if ((sec < 1.2)||(sec > 1.3))
+	// 		iters = (int)(iters * 5.0/(4.0 * sec));
+	// 	printf("%f\n", sec);
+	// } while ((sec < 1.2) || (sec > 1.3));
+
+	
+	printf("key -- %.2f (%d cycles)\n",sec,(int)tmpd);fflush(stdout);
+	outp += sprintf(outp, "Key setup: %d cycles\n\n", (int)tmpd);
+
+	/*
+	 * Get times over different lengths
+	 */
+	iters = (int)(Hz/1000);
+	i=0;
+	len = iter_list[0];
+	while (len >= 0) {
+	
+		do {
+		
+
+            ELIMAC(key, key_2, pt,iter_list[i], tag);
+
+			c = clock();
+			for (j = 0; j < iters; j++) {
+                ELIMAC(key, key_2, pt,iter_list[i], tag);
+			}
+			c = clock() - c;
+			sec = c/(double)CLOCKS_PER_SEC;
+			tmpd = (sec * Hz) / ((double)len * iters);
+			
+			if ((sec < 1.2)||(sec > 1.3))
+				iters = (int)(iters * 5.0/(4.0 * sec));
+			
+		} while ((sec < 1.2) || (sec > 1.3));
+		
+		printf("%d -- %.2f  (%6.2f cpb)\n",len,sec,tmpd);fflush(stdout);
+		outp += sprintf(outp, "%5d  %6.2f\n", len, tmpd);
+		if (len==44) {
+			ipi += 0.05 * tmpd;
+		} else if (len==552) {
+			ipi += 0.15 * tmpd;
+		} else if (len==576) {
+			ipi += 0.2 * tmpd;
+		} else if (len==1500) {
+			ipi += 0.6 * tmpd;
+		}
+		
+		++i;
+		len = iter_list[i];
+	}
+	outp += sprintf(outp, "ipi %.2f\n", ipi);
+	if (fp) {
+        fprintf(fp, "%s", outbuf);
+        fclose(fp);
+    } else
+        fprintf(stdout, "%s", outbuf);
+
+	return ((pt[0]==12) && (pt[10]==34) && (pt[20]==56) && (pt[30]==78));
+}
 
 void ELIMAC(unsigned char *K_1, unsigned char *K_2, unsigned char *M, int size, unsigned char *T){
 
@@ -209,14 +210,18 @@ void ELIMAC(unsigned char *K_1, unsigned char *K_2, unsigned char *M, int size, 
     static __m512i * plain_text_512 = (__m512i*) M;
     
     __m512i nonce;
+    __m128i nonce_128;
     __m512i S_temp;
     __m128i Tag;
     __m512i nonce_temp[1];
+    __m128i nonce_temp_128[1];
     __m512i keys_512[11];
     __m128i keys_128[11];
     __m128i keys_128_k_2[11];
     __m512i keys_0 = _mm512_setzero_si512();
+    __m128i keys_0_128 = _mm_setzero_si128();
     __m512i sum_nonce= _mm512_set_epi64(0,4, 0,4, 0,4, 0,4);
+    __m128i sum_nonce_128= _mm_set_epi32(0,0,0,1);
     union {__m128i bl128[4]; __m512i bl512;} S;
     int block_process=0;
     S.bl512 = _mm512_setzero_si512();
@@ -230,7 +235,7 @@ void ELIMAC(unsigned char *K_1, unsigned char *K_2, unsigned char *M, int size, 
     for (i = 0; i < m_blocks-1; i++){
 
         nonce_temp[0]=nonce; 
-        H(nonce_temp,  keys_512, 6, pipeline);
+        H(nonce_temp,  keys_512, 7, pipeline);
 
         plain_text_512[i]=_mm512_xor_si512(plain_text_512[i],nonce_temp[0]);
 
@@ -249,27 +254,32 @@ void ELIMAC(unsigned char *K_1, unsigned char *K_2, unsigned char *M, int size, 
         Tag=_mm_xor_si128(Tag,S.bl128[j]);
     }
 
-    int final_blocks = m_blocks_128-block_process;
-    i = i*4;
-    S.bl512=S_temp;
-    while (final_blocks){
-        nonce_temp[0]=nonce; 
-        
-        H(nonce_temp,  keys_128, 6, pipeline);
-        
-        plain_text[i]=_mm_xor_si128(plain_text[i],nonce_temp[0]);
-        
-        I(&plain_text[i],  keys_0, 4,pipeline);
+    int final_blocks = m_blocks_128-block_process-1;
 
-        Tag=_mm_xor_si128(plain_text[i],Tag);
-        nonce=_mm_add_epi64(nonce, sum_nonce);
+    i = i*4;
+    static __m128i * plain_text_128 = (__m128i*) M;
+    S.bl512=nonce;
+    nonce_128 = S.bl128[0];
+
+    while (final_blocks){
+        nonce_temp_128[0] = nonce_128;
+
+        
+        H_128(nonce_temp_128,  keys_128, 7, pipeline);
+        
+        plain_text_128[i]=_mm_xor_si128(plain_text_128[i],nonce_temp_128[0]);
+
+        I_128(&plain_text_128[i],  keys_0_128, 4,pipeline);
+
+        Tag=_mm_xor_si128(plain_text_128[i],Tag);
+        nonce_128=_mm_add_epi64(nonce_128, sum_nonce_128);
         i++;
         final_blocks--;
 
     }
     
     
-    Tag=_mm_xor_si128(Tag,S.bl128[i]);
+    Tag=_mm_xor_si128(Tag,plain_text_128[i]);
     
     AES_encrypt(Tag, &Tag, keys_128_k_2, 10);
 	_mm_store_si128 ((__m128i*)T,Tag);
